93730702cf4ceb31a394fbd8f7b374e9311597bb
Younsoo Tom Lim <tomandlim@gmail.com>
2022-01-11 18:14:19 +0900

    Added: communite-up chain

--- expenses/src/App.js
+++ expenses/src/App.js
@@ -1,9 +1,13 @@
 import NewExpense from "./components/Expense/NewExpense";
 
 function App() {
+  const addExpenseHandler = (expense) =>{
+    console.log('In App.js')
+    console.log(expense)
+  }
   return (
     <div>
-      <NewExpense></NewExpense>
+      <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
     </div>
   );
 }
