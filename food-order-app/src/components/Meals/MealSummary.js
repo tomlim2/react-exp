302ecd000d285e9d432d8a1e3cd4fa119c1994bc
Younsoo Tom Lim <tomandlim@gmail.com>
2022-02-05 16:21:27 +0900

    Added: Meals components

---
+++ food-order-app/src/components/Meals/MealSummary.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MealSummary = () => {
+  return <section><h2>Delivered To You</h2><p>Choose your favorite</p><p>All our toys are made with high quality</p></section>;
+};
+
+export default MealSummary;
