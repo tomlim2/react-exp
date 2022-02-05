de8cafedd6edb8e35604173253adfdff249c20fa
Younsoo Tom Lim <tomandlim@gmail.com>
2022-02-05 15:25:41 +0900

    Added: Cart button component

--- food-order-app/src/components/Layout/Header.js
+++ food-order-app/src/components/Layout/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import HeaderCartButton from "./HeaderCartButton";
 import perspectiveImages from "../../assets/perspectiveImg.jpg";
 import classes from "./Header.module.css";
 
@@ -7,7 +8,7 @@ const Header = () => {
     <>
       <header className={classes.header}>
         <h1>Meals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
         <img src={perspectiveImages} alt="Trees" />
