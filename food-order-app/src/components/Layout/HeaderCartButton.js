7d954c3da5a1825971f9c26019589730dbd88ee7
Younsoo Tom Lim <tomandlim@gmail.com>
2022-02-07 01:41:28 +0900

    Fixed: destructuring item cartCtx

--- food-order-app/src/components/Layout/HeaderCartButton.js
+++ food-order-app/src/components/Layout/HeaderCartButton.js
@@ -4,26 +4,31 @@ import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
-  const [btnIsHighlighted, setBtnIsHightlighted] = useState(false);
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
   const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
+  const { items } = cartCtx;
 
   const btnClasses = `${classes.button} ${
-    btnIsHighlighted ? classes.bump : null
+    btnIsHighlighted ? classes.bump : ""
   }`;
   useEffect(() => {
-    if (cartCtx.items.length === 0) {
-      setBtnIsHightlighted(true);
+    if (items.length === 0) {
+      return;
     }
+    setBtnIsHighlighted(true);
+
     const timer = setTimeout(() => {
-      setBtnIsHightlighted(false);
+      setBtnIsHighlighted(false);
     }, 300);
+
     return () => {
       clearTimeout(timer);
     };
-  }, [cartCtx]);
+  }, [items]);
+
   return (
     <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
