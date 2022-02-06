d912d61db5b3d18e1e1fcad0f7c18544bfc463a2
Younsoo Tom Lim <tomandlim@gmail.com>
2022-02-06 12:08:39 +0900

    Added: Cart component and state setting

--- food-order-app/src/components/Layout/Header.js
+++ food-order-app/src/components/Layout/Header.js
@@ -3,12 +3,12 @@ import HeaderCartButton from "./HeaderCartButton";
 import perspectiveImages from "../../assets/perspectiveImg.jpg";
 import classes from "./Header.module.css";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <>
       <header className={classes.header}>
         <h1>Meals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={perspectiveImages} alt="Trees" />
--- food-order-app/src/components/Meals/MealItem/MealItem.js
+++ food-order-app/src/components/Meals/MealItem/MealItem.js
@@ -7,7 +7,7 @@ const MealItem = (props) => {
     <li className={classes.meal}>
       <div>
         <h3>{props.name}</h3>
-        <div className={classes.meal}>{props.description}</div>
+        <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
       <div>
